05dbb0447f203ad9f0b8603358ecfe3a683453b9
unknown <mikolaura58@gmail.com>
2023-12-09 18:41:23 +0200

    add fetch data from api

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,23 @@
-<script >
+<script lang="ts">
   import { get, writable } from "svelte/store";
 
   import { onMount, setContext } from "svelte";
   /**
    * @type {string | null}
    */
-  let token;
+  let token: string | null;
   let email = "";
   let password = "";
   /**
    * @type {() => Promise<void>}
    */
-  let createUser;
+  let createUser: () => Promise<void>;
   /**
    * @type {() => Promise<void>}
    */
-  let handleLogin;
+  let handleLogin: () => Promise<void>;
+
+  let posts: {} = {};
   if (typeof localStorage !== "undefined") {
     const get = async () => {
       token = localStorage.getItem("token");
@@ -30,16 +32,15 @@
 
       const response = await fetch(
         "http://localhost:8000/api/users/me",
-        requestOptions
+        requestOptions,
       );
 
       if (!response.ok) {
       }
       const data = await response.json();
       console.log(data);
-        email = data.email
-        setContext("token",token);
-      
+      email = data.email;
+      setContext("token", token);
     };
     createUser = async () => {
       const requestOptions = {
@@ -51,7 +52,7 @@
       };
       const responses = await fetch(
         "http://localhost:8000/users",
-        requestOptions
+        requestOptions,
       );
       const data = await responses.json();
       if (!responses.ok) {
@@ -61,27 +62,55 @@
         token = data.access_token;
       }
     };
-    handleLogin = async () =>{
-        const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: JSON.stringify(`grant_type=&username=${email}&password=${password}&scope=&client_id=&client_secret=`)
+    handleLogin = async () => {
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(
+          `grant_type=&username=${email}&password=${password}&scope=&client_id=&client_secret=`,
+        ),
+      };
+      const response = await fetch(
+        "http://localhost:8000/api/token",
+        requestOptions,
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        console.log(data.detail);
+      } else {
+        token = data.access_token;
+        localStorage.setItem("token", data.access_token);
+      }
     };
-    const response = await fetch("http://localhost:8000/api/token", requestOptions);
-    const data = await response.json();
-    if(!response.ok){
-        console.log(data.detail)
-    }else{
-       
-        token = data.access_token
-         localStorage.setItem("token", data.access_token)
+    const getPosts = async () => {
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
 
-    }
-    }
+      const response = await fetch(
+        "http://localhost:8000/api/post",
+        requestOptions,
+      );
+
+      if (!response.ok) {
+      }
+      const data = await response.json();
+      posts = data;
+      console.log(Object.entries(posts));
+    };
     get();
+    onMount(getPosts);
   }
+  const users = [
+    { firstName: "John", lastName: "Doe" },
+    { firstName: "Jane", lastName: "Roe" },
+  ];
 </script>
 
 <div>
@@ -92,13 +121,15 @@
       <button on:click={createUser}>Create user</button>
     </div>
     <div>
-        <input bind:value={email} />
+      <input bind:value={email} />
       <input bind:value={password} />
       <button on:click={handleLogin}>LogIn user</button>
     </div>
   {:else if token}
     <div>
-      {email}
+      {#each Object.entries(posts) as post}
+      {post[1]["anons"]}
+      {/each}
     </div>
   {/if}
 </div>
